3d23ccbefb42004c21b12a984e54c0e92b249bb4
ShohruhUzDev <66797803+ShohruhUzDev@users.noreply.github.com>
2021-04-29 11:08:20 +0500

    add view models

---
+++ SelectListExample/ViewModels/CustomerCreateModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelectListExample.Models.ViewModels
+{
+    public class CustomerCreateModel
+    {
+        public Customer Customer { get; set; }
+        public IEnumerable<SelectListItem> Countries { get; set; }
+        public IEnumerable<SelectListItem> Cities { get; set; }
+
+    }
+}
