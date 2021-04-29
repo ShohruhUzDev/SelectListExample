2a4c8ed633876e480f924cd9b88e57851452d782
ShohruhUzDev <66797803+ShohruhUzDev@users.noreply.github.com>
2021-04-29 10:33:42 +0500

    Add DbContext for to work data base

---
+++ SelectListExample/Data/SelectListDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SelectListExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SelectListExample.Data
+{
+    public class SelectListDbContext:DbContext
+    {
+        public SelectListDbContext(DbContextOptions<SelectListDbContext>options):base(options)
+        {
+
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Country> Countries { get; set; }
+        public virtual DbSet<City> Cityies { get; set; }
+    }
+}
